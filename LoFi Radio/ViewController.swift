e066c4cd3b346cc4314299be6352f23046ded083
Matthew Lock <matthew_lock20@hotmail.com>
2022-03-16 10:52:53 +0000

    Header label added to main screen

--- LoFi Radio/ViewController.swift
+++ LoFi Radio/ViewController.swift
@@ -12,8 +12,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 219.0/255, green: 188.0/255, blue: 131.0/255, alpha: 1.0)
+        view.addSubview(header)
         
     }
 
+    let header: UILabel = {
+        var label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 100))
+        label.center = CGPoint(x: 210, y: 140)
+        label.textAlignment = .center
+        label.font = UIFont(name: "Chrome Syrup", size: 70)
+        label.textColor = UIColor(red: 25.0/255, green: 85.0/255, blue: 80.0/255, alpha: 1.0)
+        label.text = "Lofi Radio"
+        return label
+    }()
 
 }
