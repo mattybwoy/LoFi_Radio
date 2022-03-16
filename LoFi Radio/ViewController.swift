85bb0471d761fc85fbfb6cf273a57109c97a5a8a
Matthew Lock <matthew_lock20@hotmail.com>
2022-03-16 11:37:10 +0000

    Video added to main screen now playing on loop

--- LoFi Radio/ViewController.swift
+++ LoFi Radio/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    private var player: AVQueuePlayer!
+    private var playerLayer: AVPlayerLayer!
+    private var playerItem: AVPlayerItem!
+    private var playerLooper: AVPlayerLooper!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 219.0/255, green: 188.0/255, blue: 131.0/255, alpha: 1.0)
         view.addSubview(header)
+        setupVideoPlayer()
         
+    }
+    
+    func setupVideoPlayer() {
+        let path = Bundle.main.path(forResource: "LofiGirlDay", ofType: "mp4")
+        let pathURL = URL(fileURLWithPath: path!)
+        let duration = Int64( ( (Float64(CMTimeGetSeconds(AVAsset(url: pathURL).duration)) *  10.0) - 1) / 10.0 )
+
+        player = AVQueuePlayer()
+        playerLayer = AVPlayerLayer(player: player)
+        playerItem = AVPlayerItem(url: pathURL)
+        playerLooper = AVPlayerLooper(player: player, templateItem: playerItem,
+                                      timeRange: CMTimeRange(start: CMTime.zero, end: CMTimeMake(value: duration, timescale: 1)) )
+        playerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        playerLayer.frame = CGRect(x: 60, y: 220, width: 300, height: 300)
+        view.layer.insertSublayer(playerLayer, at: 1)
+        player.play()
     }
 
     let header: UILabel = {
@@ -25,5 +48,7 @@ class ViewController: UIViewController {
         label.text = "Lofi Radio"
         return label
     }()
+    
+    
 
 }
